1e148aa537219d4d4858c00e4b03dbd00aee6d77
rayhannaadisthi <114370294+rayhannaadisthi@users.noreply.github.com>
2022-09-26 09:36:42 +0700

    Add files via upload

---
+++ joptionpane.java
@@ -0,0 +1,11 @@
+package pemrogramanwebpbo;
+
+import javax.swing.JOptionPane;
+public class joptionpane {
+    public static void main(String[] args) {
+        String nama ="";
+        nama = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        String pesan = ( "belajar " + nama + " sangat mudah");
+        JOptionPane.showMessageDialog(null, pesan);        
+    }
+}
